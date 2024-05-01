80b84860390cd5022fc845edbdfd19ae85b5a09a
wdserverr <42107311+wdserverr@users.noreply.github.com>
2024-05-02 02:23:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
+  console.log(req)
 });
 
 server.listen(PORT, () => {
